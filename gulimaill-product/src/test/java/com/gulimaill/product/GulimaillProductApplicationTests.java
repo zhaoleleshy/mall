19764e5e29c061083aa9ab0ef10894c34077c3c9
zll <2743559351@qq.com>
2021-08-17 21:55:42 +0800

    test

---
+++ gulimaill-product/src/test/java/com/gulimaill/product/GulimaillProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimaill.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimaillProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
